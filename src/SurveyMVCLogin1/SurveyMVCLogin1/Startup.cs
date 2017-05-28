e1efd2f92a6e1af998487f3c4b256265109b1b7d
Steve.JIN <jinyue306@gmail.com>
2017-05-28 21:48:13 +0800

    project setup

---
+++ src/SurveyMVCLogin1/SurveyMVCLogin1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SurveyMVCLogin1.Startup))]
+namespace SurveyMVCLogin1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
