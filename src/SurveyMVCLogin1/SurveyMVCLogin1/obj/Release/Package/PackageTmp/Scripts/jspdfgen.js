4092b51f5b27b0f4accd9e68709bb3057658c7be
Steve Jin <jinyue306@gmail.com>
2017-07-09 19:21:01 +0800

    bk live login

---
+++ src/SurveyMVCLogin1/SurveyMVCLogin1/obj/Release/Package/PackageTmp/Scripts/jspdfgen.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    /*$('#cmd').click(function () {
+        var pdf = new jsPDF('p', 'pt', 'a4');
+        var options = {
+            //pagesplit: true,
+            format:"PNG",
+            background:"#FFF"
+        };
+
+        pdf.addHTML($('#page1')[0], 0, 0, options, function () { pdf.addPage(), pdf.setPage(1) });
+        
+        pdf.addHTML($('#page2')[0], 0, 0, options, function () { 
+            pdf.addPage(),
+            pdf.setPage(2),
+            pdf.save("test.pdf")
+        });
+
+        
+    });*/
+});
